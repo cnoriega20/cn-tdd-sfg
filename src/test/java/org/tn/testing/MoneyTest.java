d2d18b5a42c611c1c57a94a2d55afd0e31d14525
cnoriega20 <c79.noriega@gmail.com>
2021-03-22 01:20:55 -0500

    Degenerated object TDD 4: Refactor test function.

--- src/test/java/org/tn/testing/MoneyTest.java
+++ src/test/java/org/tn/testing/MoneyTest.java
@@ -8,9 +8,9 @@ public class MoneyTest {
     @Test
     void testMult(){
         Denomination  denomination = new Denomination(5);
-        Denomination product = denomination.times(3);
-        denomination.times(2);
+        Denomination product = denomination.times(2);
         assertEquals(10, product.amount);
+        product = denomination.times(3);
         assertEquals(15, product.amount);
     }
 }
