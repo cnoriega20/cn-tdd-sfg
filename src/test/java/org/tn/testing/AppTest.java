fb71218377ed4b5f3872e35a14ac6e3a8fe4968f
cnoriega20 <c79.noriega@gmail.com>
2021-03-22 00:31:21 -0500

    Added regular maven project and Junit5 dependencies.

---
+++ src/test/java/org/tn/testing/AppTest.java
@@ -0,0 +1,46 @@
+package org.tn.testing;
+
+
+
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+        @BeforeAll
+        static void setup(){
+            System.out.println("@BeforeAll executed");
+        }
+
+        @BeforeEach
+        void setupThis(){
+            System.out.println("@BeforeEach executed");
+        }
+
+        @AfterEach
+        void tearThis(){
+            System.out.println("@AfterEach executed");
+        }
+
+        @AfterAll
+        static void tear(){
+            System.out.println("@AfterAll executed");
+        }
+
+        @Test
+        void testCalcOne()
+        {
+            System.out.println("======TEST ONE EXECUTED=======");
+            
+        }
+
+        @Test
+        void testCalcTwo()
+        {
+            System.out.println("======TEST TWO EXECUTED=======");
+            //Assertions.assertEquals( 6 , Calculator.add(2, 4));
+        }
+
+}
