05e0a7d3a906a42d8f8b6813ed342710f1bdc3b2
cnoriega20 <c79.noriega@gmail.com>
2021-03-22 01:10:12 -0500

    Degenerated object TDD 1: Added another reference of denomination. Test and comp errors.

--- src/test/java/org/tn/testing/MoneyTest.java
+++ src/test/java/org/tn/testing/MoneyTest.java
@@ -8,7 +8,9 @@ public class MoneyTest {
     @Test
     void testMult(){
         Denomination  denomination = new Denomination(5);
+        Denomination product = denomination.times(3);
         denomination.times(2);
         assertEquals(10, denomination.amount);
+        assertEquals(15, product.amount);
     }
 }
